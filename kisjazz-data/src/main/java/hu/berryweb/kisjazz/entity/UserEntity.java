b9b87d912b000f9d617fcce024658f2368177e1f
Nándor Szabo <n.szabo@bssn-software.de>
2017-11-26 11:55:02 +0100

    Added no args and all args constructor.

--- kisjazz-data/src/main/java/hu/berryweb/kisjazz/entity/UserEntity.java
+++ kisjazz-data/src/main/java/hu/berryweb/kisjazz/entity/UserEntity.java
@@ -7,14 +7,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "user_entity")
 public class UserEntity {
 
